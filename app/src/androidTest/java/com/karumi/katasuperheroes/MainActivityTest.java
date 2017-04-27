7ecb8f692952f9bf65476895a3e20e40557042b3
Luis Ibanez <libanez@Luiss-MacBook-Pro.local>
2017-04-27 17:06:07 +0200

    Added a random test and another less random, like something useful but that I don’t want to describe right now…

--- app/src/androidTest/java/com/karumi/katasuperheroes/MainActivityTest.java
+++ app/src/androidTest/java/com/karumi/katasuperheroes/MainActivityTest.java
@@ -17,13 +17,18 @@
 package com.karumi.katasuperheroes;
 
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+
 import com.karumi.katasuperheroes.di.MainComponent;
 import com.karumi.katasuperheroes.di.MainModule;
 import com.karumi.katasuperheroes.model.SuperHero;
 import com.karumi.katasuperheroes.model.SuperHeroesRepository;
+import com.karumi.katasuperheroes.recyclerview.RecyclerViewInteraction;
+import com.karumi.katasuperheroes.ui.presenter.Presenter;
 import com.karumi.katasuperheroes.ui.view.MainActivity;
 import it.cosenonjaviste.daggermock.DaggerMockRule;
 
@@ -48,6 +53,7 @@ import static org.mockito.Mockito.when;
 @RunWith(AndroidJUnit4.class) @LargeTest public class MainActivityTest {
 
   public static SuperHero ANY_SUPER_HERO = new SuperHero("_","_",false,"_");
+  public static SuperHero ANY_AVENGER_SUPER_HERO = new SuperHero("_","_",true,"_");
 
   @Rule public DaggerMockRule<MainComponent> daggerRule =
       new DaggerMockRule<>(MainComponent.class, new MainModule()).set(
@@ -90,12 +96,68 @@ import static org.mockito.Mockito.when;
     onView(withText(ANY_SUPER_HERO.getName())).check(matches(isDisplayed()));
   }
 
+  @Test public void ShowsSuperHeroesNamesIfThereAreSuperHeroes() {
+    List<SuperHero> superHeroes = givenThereAreSuperHeroes(10);
+
+    startActivity();
+
+    RecyclerViewInteraction.<SuperHero>onRecyclerView(withId(R.id.recycler_view))
+            .withItems(superHeroes)
+            .check(new RecyclerViewInteraction.ItemViewAssertion<SuperHero>() {
+              @Override
+              public void check(SuperHero superHero, View view, NoMatchingViewException e) {
+                matches(hasDescendant(withText(superHero.getName()))).check(view, e);
+              }
+            });
+  }
+
+  @Test public void ShowsSuperHeroesBadgeIfThereAreSuperHeroesFromAvengers() {
+    List<SuperHero> superHeroes = givenThereAreSuperHeroesWithAvengers(10);
+
+    startActivity();
+
+    RecyclerViewInteraction.<SuperHero>onRecyclerView(withId(R.id.recycler_view))
+            .withItems(superHeroes)
+            .check(new RecyclerViewInteraction.ItemViewAssertion<SuperHero>() {
+              @Override
+              public void check(SuperHero superHero, View view, NoMatchingViewException e) {
+                matches(hasDescendant(withId(R.id.iv_avengers_badge))).check(view, e);
+              }
+            });
+  }
+
   private void givenThereAreNoSuperHeroes() {
     when(repository.getAll()).thenReturn(Collections.<SuperHero>emptyList());
   }
 
+  private void givenThereAreSuperHeroesWithAvengers() {
+    givenThereAreSuperHeroesWithAvengers(1);
+  }
+
+  private List<SuperHero> givenThereAreSuperHeroesWithAvengers(int heroesNumber) {
+    List<SuperHero> heroes = generateListOfSuperHeroesWithAvengers(heroesNumber);
+    when(repository.getAll()).thenReturn(heroes);
+    return heroes;
+  }
+
+  private List<SuperHero> generateListOfSuperHeroesWithAvengers(int heroesNumber) {
+    List<SuperHero> superHeroList = new ArrayList<>();
+    if(heroesNumber > 0){
+      for (int i  = 0 ; i < heroesNumber ; i ++){
+        superHeroList.add(ANY_AVENGER_SUPER_HERO);
+      }
+    }
+    return superHeroList;
+  }
+
   private void givenThereAreSuperHeroes() {
-    when(repository.getAll()).thenReturn(generateListOfSuperHeroes(1));
+    givenThereAreSuperHeroes(1);
+  }
+
+  private List<SuperHero> givenThereAreSuperHeroes(int heroesNumber) {
+    List<SuperHero> heroes = generateListOfSuperHeroes(heroesNumber);
+    when(repository.getAll()).thenReturn(heroes);
+    return heroes;
   }
 
   private List<SuperHero> generateListOfSuperHeroes(int listSize){
